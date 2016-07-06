80706cb4f2e7412bd3c3289e90a9773ad8b390a8
rect1114 <rectangle1114@gmail.com>
2016-07-06 15:39:48 +0900

    エラー関連の処理を追加

--- simplelumber.lua
+++ simplelumber.lua
@@ -1,17 +1,19 @@
 function lumber()
 	turtle.select(2)
-	count = 0
 	while turtle.compare() do
         turtle.select(3)
 		turtle.dig()
 		turtle.digUp()
 		turtle.up()
-		count = count + 1
         turtle.select(2)
 	end
-	for i=1,count do
-		turtle.down()
-	end
+
+    a,b = turtle.inspectDown()
+    while a == false do
+        turtle.down()
+        a,b = turtle.inspectDown()
+    end
+
 	if turtle.getItemCount(3) ~= 0 then
 		turtle.turnLeft()
 		for i=3,16 do
@@ -31,33 +33,43 @@ function planter()
 	end
 end
 
+-- turtle float when turtle start.
 a,b = turtle.inspectDown()
-count = 0
 if a == false then
-	while a == false do
-		turtle.down()
-		a,b = turtle.inspectDown()
-	end
+    turtle.up()
+    lumber()
 end
 
+chestSearchCount = 0
 a,b = turtle.inspect()
 if a == false then
-	while b["name"] ~= "minecraft:chest" do
+	while b["name"] ~= "minecraft:chest" and ChestSearchCount < 4 do
 		turtle.turnRight()
 		a,b = turtle.inspect()
+        chestSearchCount = ChestSearchCount + 1
 	end
-	turtle.turnRight()
-	turtle.select(1)
-	turtle.place()
+    if chestSearchCount ~= 4 then
+        turtle.turnRight()
+        turtle.select(1)
+        turtle.place()
+    else
+        print("error:please put chest on turtle's left side.")
+    end
 end
 
-while true do
+
+while true and chestSearchCount ~= 4 do
     if turtle.getFuelLevel() < 20 then
         print("LOW FUEL!")
         rs.setOutput("bottom",true)
         break
     else
-        lumber()
-        planter()
+        if turtle.getItemCount(2) == 0 then
+            print("error:please put log Block on 2 slot.")
+            break
+        else
+            lumber()
+            planter()
+        end
     end
 end
